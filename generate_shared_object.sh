51c2a36e8fec029ca5dadf7917cc6dc63b739c2b
Onix Hoque <onix.hoque@gmail.com>
2022-12-25 22:28:32 -0500

    Corrected ar command output flag

--- generate_shared_object.sh
+++ generate_shared_object.sh
@@ -7,10 +7,12 @@ cp ./bin/sFusedMMtime_fr_pt.o ./shared_object/sFusedMMtime_fr_pt.o
 cp ./kernels/lib/slibgfusedMM_pt.a ./shared_object/slibgfusedmm_pt.a
 cd shared_object/
 
-
 mkdir -p ./objects
-ar x slibgfusedmm_pt.a --output ./objects
+mv slibgfusedmm_pt.a ./objects
 cp *.o ./objects
+cd ./objects
+ar x slibgfusedmm_pt.a
+cd ..
 ar rcs libmyfusedmm.a ./objects/*.o
 gcc -O3 -march=native -std=c++11 -shared -fPIC -o libmyfusedmm_shared.so -Wl,--whole-archive libmyfusedmm.a -Wl,--no-whole-archive -Wl,-rpath='./' -lm -fopenmp 
 
