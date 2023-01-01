00923aa8216fc998b8a2ca926801d03716d442fd
Onix Hoque <onix.hooque@gmail.com>
2022-12-31 20:16:39 -0500

    Customized shared object for spmm instead of fr

--- generate_shared_object.sh
+++ generate_shared_object.sh
@@ -3,7 +3,8 @@ make
 mkdir -p ./shared_object
 
 cp ./bin/sOptFusedMM_pt.o ./shared_object/sOptFusedMM_pt.o
-cp ./bin/sFusedMMtime_fr_pt.o ./shared_object/sFusedMMtime_fr_pt.o
+cp ./bin/sFusedMMtime_spmm_pt.o ./shared_object/sFusedMMtime_spmm_pt.o
+# cp ./bin/sFusedMMtime_fr_pt.o ./shared_object/sFusedMMtime_fr_pt.o
 cp ./kernels/lib/slibgfusedMM_pt.a ./shared_object/slibgfusedmm_pt.a
 cd shared_object/
 
